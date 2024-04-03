b503d10b71fd2677eb32844838fe6c4b249d9b9b
David Buchanan <d@vidbuchanan.co.uk>
2024-04-03 14:40:16 +0000

    s

--- shellcode_example.py
+++ shellcode_example.py
@@ -12,7 +12,7 @@ shellcodes["x86_64"] = b"\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf
 # https://www.exploit-db.com/exploits/47048
 shellcodes["aarch64"] = b"\xe1\x45\x8c\xd2\x21\xcd\xad\xf2\xe1\x65\xce\xf2\x01\x0d\xe0\xf2\xe1\x8f\x1f\xf8\xe1\x03\x1f\xaa\xe2\x03\x1f\xaa\xe0\x63\x21\x8b\xa8\x1b\x80\xd2\xe1\x66\x02\xd4"
 
-shellcode = shellcode[platform.machine()]
+shellcode = shellcodes[platform.machine()]
 
 mm = mmap.mmap(-1, len(shellcode), flags=mmap.MAP_SHARED|mmap.MAP_ANONYMOUS, prot=mmap.PROT_WRITE|mmap.PROT_READ|mmap.PROT_EXEC)
 mm.write(shellcode)
